8eb6e44428740f2e978d7839c9903e8a7328b1bf
gozdesglt <sagalticigozde85@gmail.com>
2026-01-11 01:28:00 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,15 +1,29 @@
 <?php
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+date_default_timezone_set("Europe/Istanbul");
+
+$host = "ballast.proxy.rlwy.net";
+$port = 20344;
+$db = "railway";
+$user = "root";
+$pass = "TVyuCkNwmZBcGHLWXOUnDhRzdnBeOddI";
 
 try {
     $pdo = new PDO(
-        "mysql:host=ballast.proxy.rlwy.net;port=20344;dbname=railway;charset=utf8mb4",
-        "root",
-        "TVyuCkNwmZBcGHLWXOUnDhRzdnBeOddI",
-        [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false]
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
     );
-    echo "DB OK";
+
+    $pdo->exec("SET time_zone = '+03:00'");
+
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    die("DB ERROR: " . $e->getMessage());
 }
